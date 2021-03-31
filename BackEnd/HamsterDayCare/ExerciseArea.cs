c025fe696ef78dd77287e498a8a033a3f2869ba7
Johannes Posse <johannesposse@gmail.com>
2021-03-31 22:49:17 +0200

    changes

--- BackEnd/HamsterDayCare/ExerciseArea.cs
+++ BackEnd/HamsterDayCare/ExerciseArea.cs
@@ -8,10 +8,10 @@ namespace BackEnd
 {
     public class ExerciseArea
     {
-        public int ID { get; private set; }
-        public int MaxSize { get; private set; } = 6;
-        public int Size { get; private set; }
-        public virtual IList<Hamster> Hamsters { get; private set; }
+        public int ID { get; internal set; }
+        public int MaxSize { get; internal set; } = 6;
+        public int Size { get; internal set; }
+        public virtual IList<Hamster> Hamsters { get; internal set; }
 
         public ExerciseArea()
         {
@@ -19,14 +19,6 @@ namespace BackEnd
             this.MaxSize = 6;
         }
 
-        public void AddHamster(Hamster hamster)
-        {
-
-        }
-
-        public void RemoveHamster(Hamster hamster)
-        {
-
-        }
+        
     }
 }
--- BackEnd/HamsterDayCare/HamsterDayCare.cs
+++ BackEnd/HamsterDayCare/HamsterDayCare.cs
@@ -66,51 +66,85 @@ namespace BackEnd
         {
             ticksPerSecond = 1000 / ticksPerSecond;
             ticker.tick += StartThreads;
-            
+
             ticker.StartTick(ticksPerSecond, days);
         }
 
         private void StartThreads(object sender, TickEventArgs e)
         {
-            
-            
+
+
             if (e.Date.Hour == 17 & e.Date.Minute == 0)
             {
                 Console.Clear();
                 CheckOutHamstersForTheDay();
                 //e.isPaused = true;
-            }else if (e.Date.Hour == 6 & e.Date.Minute == 56)
-            {
-                Console.Clear();
             }
-            else if (e.Date.Hour >= 17 | e.Date.Hour < 7)
-            {
-                PrintEvent?.Invoke(this, new PrintEventArgs(Print(), e.Date));
-                Console.WriteLine(e.Date);
-            }
-            else if(e.Date.Hour >= 7 & e.Date.Hour <= 17)
+            else if (e.Date.Hour >= 7 & e.Date.Hour <= 17)
             {
                 Date = e.Date;
-                PrintEvent?.Invoke(this, new PrintEventArgs(Print(),e.Date));
+                PrintEvent?.Invoke(this, new PrintEventArgs(Print(), e.Date));
                 AddHamstersToCages();
+                AddHamstersToExerciseArea();
+                //RetreiveHamstersFromExtersiceArea();
             }
 
-            
+
         }
 
         private void AddHamstersToExerciseArea()
         {
-            throw new NotImplementedException();
+            var hamstersNotInCage = HDCon.Hamsters.Where(x => x.CageID == null & x.LastExercise == null).ToList();
+
+            var hamstersInCage = HDCon.Hamsters.Where(x => x.LastExercise == null & x.LastExercise != null).ToList();
+            var exerciseArea = HDCon.ExerciseArea.First();
+
+
+            if (hamstersNotInCage != null)
+            {
+                for (int i = 0; i < hamstersNotInCage.Count(); i++)
+                {
+                    if (exerciseArea.Hamsters.Count < exerciseArea.MaxSize)
+                    {
+                        exerciseArea.Hamsters.Add(hamstersNotInCage[i]);
+                        hamstersNotInCage[i].LastExercise = Date;
+                        HDCon.SaveChanges();
+                    }
+                }
+            }
+            else
+            {
+                for(int i = 0; i < hamstersInCage.Count(); i++)
+                {
+                    if(exerciseArea.Hamsters.Count < exerciseArea.MaxSize)
+                    {
+                        exerciseArea.Hamsters.Add(hamstersInCage[i]);
+                        hamstersInCage[i].LastExercise = Date;
+                        hamstersInCage[i].CageID = null;
+                        HDCon.SaveChanges();
+                    }
+                }
+            }
         }
 
-        public void AddHamstersToCages()
+        private void RetreiveHamstersFromExtersiceArea()
+        {
+            var exerciseArea = HDCon.ExerciseArea.First();
+            var hamstersInExerciseArea = exerciseArea.Hamsters.Where(x => x.LastExercise.Value.AddHours(1) >= Date).ToList();
+            var cages = HDCon.Cages;
+
+
+            //var cage = cages.AsEnumerable().FirstOrDefault(x => x.Hamsters.Count < x.MaxSize & ((x.HasFemale == hamstersInExerciseArea[i].IsFemale) | (x.Hamsters.Count < 1)));
+        }
+
+        private void AddHamstersToCages()
         {
             var hamsters = HDCon.Hamsters.OrderByDescending(x => x.IsFemale).ToList();
             var cages = HDCon.Cages;
 
             for (int i = 0; i < hamsters.Count(); i++)
             {
-                if(hamsters[i].ExerciseAreaID == null & hamsters[i].CageID == null)
+                if (hamsters[i].ExerciseAreaID == null & hamsters[i].CageID == null)
                 {
                     var cage = cages.AsEnumerable().FirstOrDefault(x => x.Hamsters.Count < x.MaxSize & ((x.HasFemale == hamsters[i].IsFemale) | (x.Hamsters.Count < 1)));
 
@@ -119,7 +153,7 @@ namespace BackEnd
                         cage.Hamsters.Add(hamsters[i]);
                         cage.HasFemale = hamsters[i].IsFemale;
 
-                        if(hamsters[i].CheckedInTime == null)
+                        if (hamsters[i].CheckedInTime == null)
                         {
                             hamsters[i].CheckedInTime = Date;
                         }
@@ -134,7 +168,9 @@ namespace BackEnd
             foreach (var ham in HDCon.Hamsters)
             {
                 ham.CageID = null;
+                ham.ExerciseAreaID = null;
                 ham.CheckedInTime = null;
+                ham.LastExercise = null;
             }
 
             foreach (var c in HDCon.Cages)
@@ -150,22 +186,33 @@ namespace BackEnd
         {
             var print = new StringBuilder();
 
-            var hamsters = HDCon.Hamsters.AsEnumerable().OrderBy(x => x.CageID).GroupBy(x => x.CageID);
 
-            foreach (var cage in hamsters)
+            var hamsters = HDCon.Hamsters.OrderBy(x => x.CageID);
+
+            foreach (var h in hamsters)
             {
-                if(cage.Key != null)
-                    print.Append("\nCage: " + cage.Key + "\n--------------------------------------------------------------\n");
-                else
-                    print.Append("\nNot in cage: " + "\n--------------------------------------------------------------\n");
-                foreach (var hamster in cage)
-                {
-                    string female = "Female";
-                    if (!hamster.IsFemale)
-                          female = "Male";
-                        print.Append($"{hamster.Name,-15}{hamster.Age,-10}{female,-10}{hamster.Ownername,-25}{hamster.CheckedInTime.ToString(),-20}{hamster.LastExercise.ToString(),-20}\n");
-                }
+                string female = "Female";
+                if (!h.IsFemale)
+                    female = "Male";
+                print.Append($"{h.CageID,-5}{h.ExerciseAreaID,-5}{h.Name,-15}{h.Age,-10}{female,-10}{h.Ownername,-25}{h.CheckedInTime.ToString(),-20}{h.LastExercise.ToString(),-20}\n");
             }
+
+            //var hamsters = HDCon.Hamsters.AsEnumerable().OrderBy(x => x.CageID).GroupBy(x => x.CageID);
+
+            //foreach (var cage in hamsters)
+            //{
+            //    if (cage.Key != null)
+            //        print.Append("\nCage: " + cage.Key + "\n--------------------------------------------------------------\n");
+            //    else
+            //        print.Append("\nNot in cage: " + "\n--------------------------------------------------------------\n");
+            //    foreach (var hamster in cage)
+            //    {
+            //        string female = "Female";
+            //        if (!hamster.IsFemale)
+            //            female = "Male";
+            //        print.Append($"{hamster.Name,-15}{hamster.Age,-10}{female,-10}{hamster.Ownername,-25}{hamster.CheckedInTime.ToString(),-20}{hamster.LastExercise.ToString(),-20}\n");
+            //    }
+            //}
 
             return print.ToString();
         }

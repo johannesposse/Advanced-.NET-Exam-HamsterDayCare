d80d325f63111a6bb8ba36be05845bdb31ba797a
Johannes Posse <johannesposse@gmail.com>
2021-04-08 13:26:01 +0200

    la till activitylogs.cs i source control

---
+++ BackEnd/Log/ActivityLog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackEnd
+{
+    public class ActivityLog
+    {
+        public int ActivityLogID { get; set; }
+        public string ActivityName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public virtual int HamsterID { get; set; }
+        public virtual Hamster Hamster { get; set; }
+
+
+        public ActivityLog(string activityName, DateTime startDate, int hamsterID)
+        {
+            this.ActivityName = activityName;
+            this.StartDate = startDate;
+            this.HamsterID = hamsterID;
+        }
+    }
+}

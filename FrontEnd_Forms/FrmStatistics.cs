27482cb57968595bcb1a63a792f5c2df71bdee84
Johannes Posse <johannesposse@gmail.com>
2021-04-11 21:36:59 +0200

    kommentarer

--- FrontEnd_Forms/FrmStatistics.cs
+++ FrontEnd_Forms/FrmStatistics.cs
@@ -20,81 +20,85 @@ namespace FrontEnd_Forms
             InitializeComponent();
         }
 
-        private void Label_NumberOfSimulations_Click(object sender, EventArgs e)
+        private void MostPopularCage(List<BackEnd.Report> reports) //metod för att hitta den mest populärar buren
         {
-
-        }
-
-        private void MostPopularCage(List<BackEnd.Report> reports)
-        {
+            //grupperar på activiteter och räknar hur många av varje aktivitet det finns, sorterar dom på den aktivitet som hänt mest
             var d = reports.GroupBy(x => x.Acticity).OrderByDescending(id => id.Count()).Select(g => new { Id = g.Key, Count = g.Count() });
+            //hämtar ut den första aktiviteten som är en cage
             var b = d.Where(x => x.Id.Contains("Cage")).FirstOrDefault();
+
+            //skriver ut den mest populära buren och hur många gången den besökts
             lbl_MostPopularCage.Text = b.Id.Trim();
             lbl_MostPopularCageID.Text = b.Count.ToString() + " visits";
         }
 
-        private void MosteExercisedHamster(List<BackEnd.Report> reports)
+        private void MosteExercisedHamster(List<BackEnd.Report> reports) //metod för att hitta den hamstern som tränat mest
         {
+            //hämtar de aktiviteter som är exersice och grupperar på hamstrarnas namn
             var hamsters = reports.Where(x => x.Acticity == " Exercise").GroupBy(x => x.Name).Select(x => new { Name = x.Key, Count = x.Count() });
+
+            //sorterar så att hamstern som tränat mest kommer först och hämtar ut det första inlägget
             var mostExercised = hamsters.OrderByDescending(x => x.Count).FirstOrDefault();
+
+            //skriver ut den hamstern som tränat mest och vad den heter
             lbl_MosteExercisedTimes.Text = "Has exercised " + mostExercised.Count.ToString() + " times";
             lbl_MostExercisedName.Text = mostExercised.Name;
         }
 
-        private void AverageWaitingTimeToExercise(List<BackEnd.Report> reports)
+        private void AverageWaitingTimeToExercise(List<BackEnd.Report> reports) //metod för att hitta hur länge hamstrarna fått vänta i snitt på att träna
         {
-            var options = hamsterDayCare.ShowPreviousResults();
+            var options = hamsterDayCare.ShowPreviousResults(); //hämtar ut hur många gånger simulationen körts
 
-            TimeSpan start = TimeSpan.Parse("07:00:00");
+            TimeSpan start = TimeSpan.Parse("07:00:00"); //startdatum
 
-            var exercise = reports.Where(x => x.Acticity == " Exercise").OrderBy(x => x.Start);
+            var exercise = reports.Where(x => x.Acticity == " Exercise").OrderBy(x => x.Start); //hämtar ut de aktiviteter som är extersice och sorterar så tidigast kommer först
 
             int timeDifference = 0;
-            int num = 30 * (options.Length - 1);
+            int num = 30 * (options.Length - 1); //kollar hur många simulationer som gjort, tar bort en (total.txt), gånger 30 för att plocka fram den första träningen de gjort alla dagar
                 
-            if (options.Any())
+            if (options.Any()) //kollar om det finns några simuleringar rapporterade
             {
-                var firstExersice = exercise.Take(num);
+                var firstExersice = exercise.Take(num); //hämtar ut de antalet första träningar 
 
-                foreach (var first in firstExersice)
+                foreach (var first in firstExersice) //loopar igenom alla träningar
                 {
-                    var timeSpanDifference = first.Start.TimeOfDay - start;
-                    timeDifference += timeSpanDifference.Hours;
+                    var timeSpanDifference = first.Start.TimeOfDay - start; //kollar skillnaden mellan 07.00 och när träningen startade
+                    timeDifference += timeSpanDifference.Hours; //lägger till tidsskillnaden till en int
                 }
 
-                double averageTime = timeDifference / num;
-                lbl_AverageWatingToExercise.Text = averageTime + " hours";
+                double averageTime = timeDifference / num; //tar fram medelvärdet
+                lbl_AverageWatingToExercise.Text = averageTime + " hours"; //skriver ut medelvärdet
             }
-            else
+            else //om det inte finns några rapporter 
             {
                 lbl_AverageWatingToExercise.Text = "N/A";
             }  
         }
 
-        private void HamsterStatestic(List<BackEnd.Report> reports)
-        {
-            StringBuilder info = new StringBuilder();
+        //private void HamsterStatestic(List<BackEnd.Report> reports)
+        //{
+        //    StringBuilder info = new StringBuilder();
 
-            var Exersice = reports.Where(x => x.Acticity == " Exercise").GroupBy(x => x.Name).Select(x => new { Name = x.Key, Count = x.Count() });
-            var Cage = reports.Where(x => x.Acticity.Contains(" Cage:")).GroupBy(x => x.Name);
+        //    var Exersice = reports.Where(x => x.Acticity == " Exercise").GroupBy(x => x.Name).Select(x => new { Name = x.Key, Count = x.Count() });
+        //    var Cage = reports.Where(x => x.Acticity.Contains(" Cage:")).GroupBy(x => x.Name);
 
             
 
-            txt_HamsterStatic.Text = info.ToString();
-        }
+        //    txt_HamsterStatic.Text = info.ToString();
+        //}
 
-        private void AverageTimesExercisedPerDay(List<BackEnd.Report> reports)
+        private void AverageTimesExercisedPerDay(List<BackEnd.Report> reports) //metod för att visa i snitt hur många gånger de tränar per dag
         {
-            var options = hamsterDayCare.ShowPreviousResults();
-            var exercise = reports.Where(x => x.Acticity == " Exercise");
+            var options = hamsterDayCare.ShowPreviousResults(); //hämtar ut hur många gånger simulationen körts
+            var exercise = reports.Where(x => x.Acticity == " Exercise"); //hämar ut de aktiviteter som är exercise
 
-            if (exercise.Any())
+            if (exercise.Any()) //om det inte är tomt
             {
-                var exCount = exercise.Count();
-                double times = Convert.ToDouble(exCount) / 30;
-                lbl_AverageTimesExercisedPerDayNum.Text = times.ToString();
+                var exCount = exercise.Count(); //räknar ihop hur många exercise det finns
+                double times = Convert.ToDouble(exCount) / 30; //delar på 30 (finns 30 hamstrar)
+                lbl_AverageTimesExercisedPerDayNum.Text = times.ToString(); //skriver ut snittvärdet
             }
-            else
+            else //om det inte finns några tidigare simuleringar
             {
                 lbl_AverageTimesExercisedPerDayNum.Text = "N/A";
             }
@@ -103,15 +107,16 @@ namespace FrontEnd_Forms
         private void FrmStatistics_Load(object sender, EventArgs e)
         {
 
-            if (File.Exists(@"..\..\..\..\Logs\total.txt"))
+            if (File.Exists(@"..\..\..\..\Logs\total.txt")) //kollar om total.txt dokumentet finns
             {
-                string path = @"..\..\..\..\Logs\total.txt";
-                List<string> input = File.ReadAllLines(path).ToList();
-                List<BackEnd.Report> reports = new List<BackEnd.Report>();
+                string path = @"..\..\..\..\Logs\total.txt"; //sökväg för total.txt dokumentet
+                List<string> input = File.ReadAllLines(path).ToList(); //läser in total.txt till en lista av strings
+                List<BackEnd.Report> reports = new List<BackEnd.Report>(); //gör en ny instans av reports
 
-                foreach (var i in input)
+                foreach (var i in input) //loopar igenom listan
                 {
-                    var data = i.Split(",");
+                    var data = i.Split(","); //gör en array av varje inlägg i listan som splittas på ","
+                    //skapar en ny rapport med datan i arrayen
                     reports.Add(new BackEnd.Report(int.Parse(data[0]), data[1], data[2], data[3], DateTime.Parse(data[4]), DateTime.Parse(data[5])));
                 }
 
@@ -119,9 +124,8 @@ namespace FrontEnd_Forms
                 MosteExercisedHamster(reports);
                 AverageWaitingTimeToExercise(reports);
                 AverageTimesExercisedPerDay(reports);
-                HamsterStatestic(reports);
             }
-            else
+            else //om total.txt inte finns så sätts default värden
             {
                 lbl_MostPopularCage.Text = "N/A";
                 lbl_MostPopularCageID.Text = "N/A";
--- FrontEnd_Forms/frmSimulation.cs
+++ FrontEnd_Forms/frmSimulation.cs
@@ -17,39 +17,39 @@ namespace FrontEnd_Forms
     {
         static BackEnd.HamsterDayCare hamsterDayCare = new BackEnd.HamsterDayCare();
         static Thread threadOne;
-        string data;
+        string data; //för att kunna visa datan som skickas från backend utan att det ska bli konflikter med trådar
         DateTime Date;
         public FrmSimulation()
         {
             InitializeComponent();
-            hamsterDayCare.ReportEvent += ShowReport;
-            data = "";
+            hamsterDayCare.ReportEvent += ShowReport; //sätter Showreport som lyssnare på report eventet
+            data = ""; //nollställer
         }
 
-        private void ShowReport(object sender, ReportEventArgs e)
+        private void ShowReport(object sender, ReportEventArgs e) //anropas när en ny rapport genereras
         {
-            MessageBox.Show("A new report has been created. Go to the reports tab to check it out");
+            MessageBox.Show("A new report has been created. Go to the reports tab to check it out"); //en messagebox visas
             
-            data = "";
+            data = ""; //nollställer data
         }
 
-        private void ReceiveData(object sender, PrintEventArgs e)
+        private void ReceiveData(object sender, PrintEventArgs e) //anropas varje tick för att kunna skriva ut det som hänt
         {
-            data = e.Data;
+            data = e.Data; //sätter värdena
             Date = e.Date;
         }
 
-        private void StartSimulation()
+        private void StartSimulation() //metod för att starta ny simulering
         {
             
-            BackEnd.HamsterDayCare.PrintEvent += ReceiveData;
+            BackEnd.HamsterDayCare.PrintEvent += ReceiveData; //sätter ReceiveData som lyssnare på PrintEvent varje tick
 
             int numDays;
             int numSpeed;
 
-            string path = @"..\..\..\..\config.txt";
+            string path = @"..\..\..\..\config.txt"; //sökväg för config filen
 
-            try
+            try //försöker läsa in configfilen
             {
                 var config = File.ReadAllLines(path).ToList();
                 var days = config[1].Split(",");
@@ -57,41 +57,41 @@ namespace FrontEnd_Forms
                 var speed = config[2].Split(",");
                 numSpeed = int.Parse(speed[1]);
             }
-            catch
+            catch //gick inte det så sätts defaultvärden och en messagebox visas
             {
                 MessageBox.Show("The config file was corrupted.\nRunning simulation at default values");
                 numDays = 1;
                 numSpeed = 3;
             }
 
-            hamsterDayCare.StartSimulation(numDays, numSpeed);
+            hamsterDayCare.StartSimulation(numDays, numSpeed); //startar en ny simulering med de inlästa värderna från config fil (eller defualtvärden)
 
             
-            data = "";
-            threadOne = null;
+            data = ""; //nollställer
+            threadOne = null; //när simuleringen är klar, så nullas tråden
         }
 
         private void FrmSimulation_Load(object sender, EventArgs e)
         {
-            threadOne = null;
+            threadOne = null; //tråden nullas vid inladdning av formulär
         }
 
         private void TimerGetData_Tick(object sender, EventArgs e)
         {
-            
+            //en timer som ligger och tickar och sätter textbox och label till data(string) och date(datum)
             textBox1.Text = data;
             Label_Date.Text = Date.ToString("yyyy:MM:dd hh:mm:ss");
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e) //knapp för att starta simulering
         {
-            if (threadOne == null)
+            if (threadOne == null) //kollar om tråden inte är aktiv
             {
-                threadOne = new Thread(new ThreadStart(StartSimulation));
-                threadOne.Start();
-                TimerGetData.Start();
+                threadOne = new Thread(new ThreadStart(StartSimulation)); //initialiserar tråden Startsimulation metoden
+                threadOne.Start(); //startar tråden
+                TimerGetData.Start(); //startar timern som uppdaterar och visar data
             }
-            else
+            else //är den aktiv visas en messagebox
             {
                 MessageBox.Show("Stop that");
             }
